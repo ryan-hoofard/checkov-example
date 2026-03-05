6f3a4e5308b55bf11bfe5b79e67a9756588b341c
ryan-hoofard <ryan.hoofard@arb.ca.gov>
2026-03-05 11:13:13 -0800

    test: getting rid of s3 lifecycle and versioning

--- ApplicationResources/s3.tf
+++ ApplicationResources/s3.tf
@@ -27,24 +27,24 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "s3_objects_transition" {
-  bucket = aws_s3_bucket.this.id
-  rule{
-    id     = "delete rule for checkov file versions"
-    status = "Enabled"
-    noncurrent_version_expiration {
-      noncurrent_days = 30
-    }
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 7
-    }
-  }
-}
+# resource "aws_s3_bucket_lifecycle_configuration" "s3_objects_transition" {
+#   bucket = aws_s3_bucket.this.id
+#   rule{
+#     id     = "delete rule for checkov file versions"
+#     status = "Enabled"
+#     noncurrent_version_expiration {
+#       noncurrent_days = 30
+#     }
+#     abort_incomplete_multipart_upload {
+#       days_after_initiation = 7
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.this.id
+# resource "aws_s3_bucket_versioning" "versioning" {
+#   bucket = aws_s3_bucket.this.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
